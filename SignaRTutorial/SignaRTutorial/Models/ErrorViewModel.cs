398ff86c76ab585165958bd531eb0c201ab4f6f8
E-Ibiri <emiakpor.ibiri@cyphercrescent.com>
2022-10-07 13:11:15 +0100

    Added more files

---
+++ SignaRTutorial/SignaRTutorial/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SignaRTutorial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
